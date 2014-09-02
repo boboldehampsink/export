05305f39d0bbe026d937cdcb2b258d7d0d850143
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-09-02 11:24:05 +0200

    Fixed a bug with lightswitch "no" values.

--- services/ExportService.php
+++ services/ExportService.php
@@ -253,14 +253,14 @@ class ExportService extends BaseApplicationComponent
                 // Make data human readable
                 switch($data) {
                 
+                    case "0":
+                        $data = Craft::t("No");
+                        break;
+                
                     case "1":
                         $data = Craft::t("Yes");
                         break;
                         
-                    case "2":
-                        $data = Craft::t("No");
-                        break;
-                
                 }
             
                 break;
@@ -314,12 +314,12 @@ class ExportService extends BaseApplicationComponent
                         // Make data human readable
                         switch($data) {
                         
+                            case "0":
+                                $data = Craft::t("No");
+                                break;
+                        
                             case "1":
                                 $data = Craft::t("Yes");
-                                break;
-                                
-                            case "2":
-                                $data = Craft::t("No");
                                 break;
                         
                         }
