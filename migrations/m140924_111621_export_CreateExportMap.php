47087118dc370b57ad9637d793c33212cdc08350
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-09-24 13:18:46 +0200

    Added a migration for upgrading

---
+++ migrations/m140924_111621_export_CreateExportMap.php
@@ -0,0 +1,26 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m140924_111621_export_CreateExportMap extends BaseMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 *
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+	
+	    // Create the craft_export_map table
+	    craft()->db->createCommand()->createTable('export_map', array(
+	    	'settings' => array('column' => 'text'),
+	    	'map'      => array('column' => 'text'),
+	    ), null, true);
+	
+		return true;
+		
+	}
+}
