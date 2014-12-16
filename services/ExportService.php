3df74eea39533794cb495c34a75c41f55bdc5251
Bob Olde Hampsink <b.oldehampsink@itmundi.nl>
2014-12-16 10:52:01 +0100

    Fixed a bug when parsing objects as string

--- services/ExportService.php
+++ services/ExportService.php
@@ -307,7 +307,7 @@ class ExportService extends BaseApplicationComponent
         
         // If it's an object, make it a string
         if(is_object($data)) {
-            $data = StringHelper::arrayToString(array_filter(get_object_vars($data), ', '));
+            $data = StringHelper::arrayToString(array_filter(get_object_vars($data)), ', ');
         }
                                
         return $data;
